19489519f7ec6b32d5ac38970ae220b512b44eb0
dmolina79 <doug.molinam@gmail.com>
2020-02-06 22:29:56 -0600

    refactor to use sampleJson

--- src/service/incidentService.js
+++ src/service/incidentService.js
@@ -1,31 +1,44 @@
 import axios from 'axios';
 import _ from 'lodash';
 import { apiUrl } from '../config';
+import sampleIncident from './sampleJson/F01705150090.json';
+
+const parseIncidentData = (incident) => {
+    return {
+        lat: _.get(incident, 'address.latitude'),
+        lng: _.get(incident, 'address.longitude'),
+    }
+}
 
 
+// emulating an API call
 export const fetchIncident = async (incidentId) => {
-    console.log('fetchIncident', incidentId);
     if (!incidentId || isNaN(incidentId)) {
         throw new Error('Expected a valid incident number');
     }
-    const queryUrl = `${apiUrl}${incidentId}?f=json&pretty=true`;
 
-    const config = {
-        headers: {
-            "Content-Type": "application/json",
-            // This line was having CORS issues from chrome since API is http
-            // "Access-Control-Allow-Origin": "*"
-        },
-        crossdomain: true,
-    }    
-    console.log('queryUrl', queryUrl);
-    const res = await axios.get(queryUrl, config);
+    const parsedRes =  parseIncidentData(sampleIncident);
 
-    console.log('res', res);
+    return parsedRes;
+}
 
-    if (_.get(res, 'data')) {
-        return res.data;
-    } else {
-        return null;
-    }    
+
+const fetchEnerApi = async () => {
+        const queryUrl = `${apiUrl}?f=json&pretty=true`;
+
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+                // This line was having CORS issues from chrome since API is http
+                // "Access-Control-Allow-Origin": "*"
+            },
+            crossdomain: true,
+        }    
+        const res = await axios.get(queryUrl, config);
+    
+        if (_.get(res, 'data')) {
+            return res.data;
+        } else {
+            return null;
+        }   
 }
