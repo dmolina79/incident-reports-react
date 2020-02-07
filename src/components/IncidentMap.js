2efd38e8ce81ef5bf719d871a967555bda24dd3e
dmolina79 <doug.molinam@gmail.com>
2020-02-06 22:37:25 -0600

    add skeleton components. Remove Api key

---
+++ src/components/IncidentMap.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class IncidentMap extends Component {
+    render () {
+        return (
+            <div>
+                map goes here
+            </div>
+        )
+    }
+}
+
+export default IncidentMap;
--- src/components/main.js
+++ src/components/main.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
+import SearchPanel from './SearchPanel';
+import IncidentMap from './IncidentMap';
 import { fetchIncident } from '../service/incidentService';
 
 class Main extends Component {
+    state = {
+        notFound: false,
+
+    }
     async componentDidMount() {
-        await fetchIncident(1);
+        const mockIncidentId = 1;
+        const { lat, lng } = await fetchIncident(mockIncidentId);
+
+        // if we dont have lat or lng incident not found
+        if (!lat || !lng) {
+            this.setState({ notFound: true });
+        } else {
+
+        }
     }
     render () {
         return (
             <div>
-                Main components
+                Main component
+                <SearchPanel />
+                <IncidentMap />
             </div>
         )
     }
